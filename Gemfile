95ef54c84be0b1b8f174f4d3f19b858df5378084
Janet Mardjuki <mardjuki@sfu.ca>
2016-06-14 16:52:50 -0700

    Completed the user modal * Followed the Sign Up tutorial (Chapter 6)

--- Gemfile
+++ Gemfile
@@ -4,6 +4,10 @@ ruby '2.2.4'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
+
+# Encrypt the password
+gem 'bcrypt', '3.1.7'
+
 # Use postgresql as the database for Active Record
 gem 'pg'
 gem 'rails_12factor', group: :production
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614041053) do
+ActiveRecord::Schema.define(version: 20160614231106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20160614041053) do
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   create_table "widgets", force: :cascade do |t|
     t.string   "name"
