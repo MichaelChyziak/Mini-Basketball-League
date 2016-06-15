ea584be535eaf694c112d02bb6df4a46e19e2400
cmpt276 <mchyziak@sfu.ca>
2016-06-15 08:46:36 -0700

    Removed widgets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614231106) do
+ActiveRecord::Schema.define(version: 20160615153855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,13 +38,5 @@ ActiveRecord::Schema.define(version: 20160614231106) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-
-  create_table "widgets", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "stock"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
