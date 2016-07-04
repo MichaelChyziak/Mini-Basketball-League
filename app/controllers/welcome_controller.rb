d42723da63d6d4bd10462563124eff5920e8100e
Michael Chyziak <mchyziak@sfu.ca>
2016-07-03 21:22:51 -0700

    Added calculations of games played/won/lost/tied and points in Standings

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -21,20 +21,24 @@ class WelcomeController < ApplicationController
     @teams_approved_gold = Team.where(:status =>1, :league =>"Gold")
 
   end
-  
+
   def schedule2
     @teams = Team.all
     @teams_approved_silver = Team.where(:status =>1, :league =>"Silver")
   end
-  
+
   def schedule3
     @teams = Team.all
     @teams_approved_bronze = Team.where(:status =>1, :league =>"Bronze")
   end
-  
-  def standings
-     @teams = Team.all
 
+  def standings
+     @teams_gold = Team.where(:status =>1, :league =>"Gold")
+     @teams_silver = Team.where(:status =>1, :league =>"Silver")
+     @teams_bronze = Team.where(:status =>1, :league =>"Bronze")
+     @league_gold = League.where(name: "Gold").first
+     @league_silver = League.where(name: "Silver").first
+     @league_bronze = League.where(name: "Bronze").first
   end
 
 end
