ea0208bd3be0ca8e8d0cf47457aa48c026ea4f22
Ray <rliu825@gmail.com>
2016-07-10 18:18:33 +0000

    ray test

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    log_in_as(@user)
+    assert is_logged_in?
+  end
+  
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+  
+  test "should get schedule" do
+    get :schedule
+    assert_response :success
+  end
+  
+  
+  
 end
