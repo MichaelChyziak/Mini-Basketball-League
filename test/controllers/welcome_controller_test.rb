05512df395de3e6bb485b99e6044eb7e9ca29ddc
Ray <rliu825@gmail.com>
2016-07-11 00:08:39 +0000

    welcome test routes done

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,12 +2,11 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
   def setup
-    @user = users(:michael)
-    log_in_as(@user)
-    assert is_logged_in?
+    @user = User.create(username:"ray",email:"ray@example.com",password:'password',admin:true)
   end
-  
+#when logged in  
   test "should get home" do
+    session[:user_id] = @user.id
     get :home
     assert_response :success
   end
@@ -16,12 +15,62 @@ class WelcomeControllerTest < ActionController::TestCase
     get :about
     assert_response :success
   end
-  
-  test "should get schedule" do
+
+  test "should get shedule" do
+    session[:user_id] = @user.id
     get :schedule
     assert_response :success
   end
   
+  test "should get shedule2" do
+    session[:user_id] = @user.id
+    get :schedule2
+    assert_response :success
+  end
+
+  test "should get shedule3" do
+    session[:user_id] = @user.id
+    get :schedule3
+    assert_response :success
+  end
+
+  test "should get standings" do
+    session[:user_id] = @user.id
+    get :standings
+    assert_response :success
+  end
+  
+#not logged in  
+
+  test "should not get home" do
+    get :home
+    assert_redirected_to root_path
+  end
+  
+  test "should STILL get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should not get shedule" do
+    get :schedule
+    assert_redirected_to root_path
+  end
+  
+  test "should not get shedule2" do
+    get :schedule2
+    assert_redirected_to root_path
+  end
+
+  test "should not get shedule3" do
+    get :schedule3
+    assert_redirected_to root_path
+  end
+
+  test "should not get standings" do
+    get :standings
+    assert_redirected_to root_path
+  end
   
   
 end
