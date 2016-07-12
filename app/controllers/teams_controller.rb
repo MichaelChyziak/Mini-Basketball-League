b5f156f84513ab5392b0b237ce687efa4548dc6c
Michael Chyziak <mchyziak@sfu.ca>
2016-07-11 20:45:04 -0700

    Fixed captain being able to remove themselves from their team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -97,11 +97,16 @@ class TeamsController < ApplicationController
   def remove_user
     @team = Team.find(1)
     if current_user.id == @team.captain_id #allow only captian to remove a user from the team
-      @team.players_id.delete(params[:player_id].to_i)
-      @team.save
-      User.find(params[:player_id]).update_attribute(:team_id, -1)
-      redirect_to action: "captain_team"
-      flash[:warning] = "Successfully removed user from team."
+      if (params[:player_id] == current_user.id)
+        @team.players_id.delete(params[:player_id].to_i)
+        @team.save
+        User.find(params[:player_id]).update_attribute(:team_id, -1)
+        redirect_to action: "captain_team"
+        flash[:warning] = "Successfully removed user from team."
+      else
+        redirect_to action: "captain_team"
+        flash[:warning] = "Cannot remove yourself from the team."
+      end
     else
       redirect_to "/home"
       flash[:warning] = "Only the team's captain can do that action."
